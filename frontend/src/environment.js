0c67ab100ca58d3e4a885f93ad34e4b631b718f6
Abhishekx9 <xabhishek.kumar9@gmail.com>
2025-08-19 13:15:12 +0530

    Deploying Frontend

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
+    "https://apna-video-call-backend-8m8y.onrender.com" :
 
     "http://localhost:8000"
 
